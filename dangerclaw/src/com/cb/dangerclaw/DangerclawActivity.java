5aa45c13fedc868fa20b9704045e62c4f56fd1a1
Brandon Logan <johnbrandonlogan@gmail.com>
2011-10-13 00:44:44 -0500

    first update

---
+++ dangerclaw/src/com/cb/dangerclaw/DangerclawActivity.java
@@ -0,0 +1,13 @@
+package com.cb.dangerclaw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DangerclawActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
